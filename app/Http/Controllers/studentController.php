9b2230c5192b7a43a4b82796b39ac6f8f7bfa59b
kian <kianoosh09@gmail.com>
2020-05-19 18:21:34 +0430

    v2

---
+++ app/Http/Controllers/studentController.php
@@ -0,0 +1,153 @@
+<?php
+
+ 
+
+namespace App\Http\Controllers;
+
+ 
+
+use illuminate\Http\request;
+
+ 
+
+class studentController extends Controller
+
+{
+
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return void
+
+     */
+
+    public function __construct()
+
+    {
+
+        //
+
+    }
+
+ 
+
+    //
+
+    public function read($id){
+
+        switch ($id){
+
+            case 1:
+
+             return [
+
+                'id'=>1,
+
+                'name'=>'kian',
+
+                'family'=>'omidi',
+
+            ];
+
+                break;
+
+             case 2:
+
+                return [
+
+                     'id'=>2,
+
+                     'name'=>'ali',
+
+                     'family'=>'armani',
+
+             ];
+
+                break;
+
+                default:
+
+                return 'others';
+
+ 
+
+        }
+
+    }
+
+ 
+
+    public function list(){
+
+        //return 'hello';
+
+        $students = [];
+
+              //connect to DB
+
+ 
+
+              $students []= [
+
+                  'id'=>1,
+
+                  'name'=>'kian',
+
+                  'family'=>'omidi',
+
+              ];
+
+              $students []= [
+
+                'id'=>2,
+
+                'name'=>'ali',
+
+                'family'=>'armani',
+
+            ];
+
+            $students []= [
+
+                'id'=>3,
+
+                'name'=>'mamad',
+
+                'family'=>'mohamadi',
+
+            ];
+
+        return $students;
+
+ 
+
+    }
+
+ 
+
+    public function create(request $request){
+
+        $id= $request->id;
+
+        $name= $request->name;
+
+        $family= $request->family;
+
+        //DB
+
+ 
+
+        $result = 'student id=' . $id . 'was created!!';
+
+        return $result;
+
+ 
+
+ 
+
+    }
+
+}
